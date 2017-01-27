305632f1ca9c97e6989220bb0307ed7004ce54e5
Bartłomiej Skrobol <skrobolbartlomiej@gmail.com>
2017-01-27 16:29:28 +0100

    refaktor plus sprawozdania

--- interpolacja/Interpolation.cpp
+++ interpolacja/Interpolation.cpp
@@ -1,6 +1,41 @@
 #include "Interpolation.h"
 #include <iostream>
 
+void parallelNodes(size_t nodesAmount, double downRange, double upRange, std::vector<double>& x)
+{
+	x.clear();
+	double h = (upRange - downRange) / nodesAmount;
+	for (size_t i = 0; i <= nodesAmount; ++i)
+	{
+		x.push_back(downRange + i*h);
+	}
+}
+
+void czebyszewNodes(size_t nodesAmount, double downRange, double upRange, std::vector<double>& x)
+{
+	x.clear();
+	double temp = 0.0;
+	double tmp = 0.0;
+	for (size_t i = 0; i <= nodesAmount; ++i)
+	{
+		tmp = (2 * (double)i + 1);
+		tmp /= (2 * nodesAmount + 2);
+		tmp *= PI;
+		tmp = cos(tmp);
+		temp = (upRange - downRange) / 2;
+		temp *= cos(((2 * (double)i + 1) / (2 * nodesAmount + 2))*PI);
+		//temp *= tmp;
+		temp += (downRange + upRange) / 2;
+		x.push_back(temp);
+	}
+}
+
+double absoluteError(double fx, double Lx)
+{
+	return abs(fx - Lx);
+}
+
+
 NewtonInterpolation::NewtonInterpolation(const std::vector<double>& x, const std::vector<double>& y):mX(x),mY(y)
 {
 	mNodes = mX.size()-1;
--- interpolacja/Interpolation.h
+++ interpolacja/Interpolation.h
@@ -3,6 +3,12 @@
 #include "mathUtils.h"
 #include <algorithm>
 
+const double PI = 3.141592653589793238463;
+
+
+void parallelNodes(size_t nodesAmount, double downRange, double upRange, std::vector<double>& x);
+void czebyszewNodes(size_t nodesAmount, double downRange, double upRange, std::vector<double>& x);
+
 ///blad bezwzgledny
 double absoluteError(double fx, double Lx);
 
